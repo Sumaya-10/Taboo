8bbec063809f04560e8e2755aa23768c22a4c012
Sumaya-10 <sumayam-ab108@code.edu.az>
2024-12-19 14:34:23 +0400

    lalay

---
+++ Taboooooooooooo/Taboooooooooooo/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Taboooooooooooo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GameController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Method()
+    {
+        return BadRequest("Salam");
+    }
+}
---
+++ Taboooooooooooo/Taboooooooooooo/Entities/Language.cs
@@ -0,0 +1,9 @@
+﻿namespace Taboooooooooooo.Entities
+{
+    public class Language
+    {
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Icon { get; set; } = null!;
+    }
+}
